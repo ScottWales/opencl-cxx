7755e5c83fed4ccd14be295b65f84aac11baa0d8
Scott Wales <wales@physics.usyd.edu.au>
2010-11-17 11:22:28 +1100

    Platform wrapper for opencl headers

---
+++ include/opencl-cxx/opencl.hpp
@@ -0,0 +1,15 @@
+#ifndef OPENCL_CXX_OPENCL_HPP
+#define OPENCL_CXX_OPENCL_HPP 
+
+// Copyright Scott Wales 2010
+// Distributed under the Boost Software Licence, Version 1.0.
+// (See accompanying file LICENCE_1_0.txt or copy at
+// http://www.boost.org/LICENCE_1_0.txt)
+
+#ifdef __APPLE__
+#include <OpenCL/opencl.h>
+#else
+#include <CL/opencl.h>
+#endif
+
+#endif 
