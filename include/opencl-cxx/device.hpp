844bd7604152676301660c119d8c17fd5228f40d
Scott Wales <wales@physics.usyd.edu.au>
2010-11-17 11:46:22 +1100

    Device wrapper

--- include/opencl-cxx/device.hpp
+++ include/opencl-cxx/device.hpp
@@ -1,9 +1,31 @@
-#ifndef OPENCL_CXX_HPP
-#define OPENCL_CXX_HPP 
+#ifndef OPENCL_CXX_DEVICE_HPP
+#define OPENCL_CXX_DEVICE_HPP 
 
 // Copyright Scott Wales 2010
 // Distributed under the Boost Software Licence, Version 1.0.
 // (See accompanying file LICENCE_1_0.txt or copy at
 // http://www.boost.org/LICENCE_1_0.txt)
+
+#include <opencl-cxx/opencl.hpp>
+#include <opencl-cxx/devicetype.hpp>
+#include <vector>
+
+namespace OpenCL {
+    /// An OpenCL device
+    class Device {
+        public:
+            /// All available devices on the default platform
+            static std::vector<Device> All(enum DeviceType::type type 
+                    = DeviceType::Default);
+            /// Default device on the default platform
+            static Device Default(enum DeviceType::type type
+                    = DeviceType::Default);
+        private:
+            Device(cl_device_id cl_impl):cl_impl(cl_impl){}
+            Device(); // Not implemented
+
+            cl_device_id cl_impl;
+    }
+}
 
 #endif 
--- include/opencl-cxx/platform.hpp
+++ include/opencl-cxx/platform.hpp
@@ -1,9 +1,38 @@
-#ifndef OPENCL_CXX_HPP
-#define OPENCL_CXX_HPP 
+#ifndef OPENCL_CXX_PLATFORM_HPP
+#define OPENCL_CXX_PLATFORM_HPP 
 
 // Copyright Scott Wales 2010
 // Distributed under the Boost Software Licence, Version 1.0.
 // (See accompanying file LICENCE_1_0.txt or copy at
 // http://www.boost.org/LICENCE_1_0.txt)
+
+#include <opencl-cxx/opencl.hpp>
+#include <opencl-cxx/devicetype.hpp>
+#include <vector>
+
+namespace OpenCL {
+    class Device;
+    /// An OpenCL platform
+    class Platform {
+        public:
+            /// All available platforms
+            static std::vector<Platform> All();
+            /// Default platform
+            static Platform Default();
+            
+            /// Get all devices on the platform
+            std::vector<Device> getAllDevices(enum DeviceType::type type
+                    = DeviceType::Default);
+            /// Get the default device on the platform
+            Device getDefaultDevice(enum DeviceType::type type
+                    = DeviceType::Default);
+        private:
+            Platform(cl_platform_id cl_impl):cl_impl(cl_impl){}
+            Platform(); // Not implemented
+
+            cl_platform_id cl_impl;
+    }
+}
+
 
 #endif 
