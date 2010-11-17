b1703fa2f823f3e7b725eb17d439f87bcf3622bd
Scott Wales <wales@physics.usyd.edu.au>
2010-11-17 11:35:29 +1100

    Header skeletons

---
+++ include/opencl-cxx/device.hpp
@@ -0,0 +1,9 @@
+#ifndef OPENCL_CXX_HPP
+#define OPENCL_CXX_HPP 
+
+// Copyright Scott Wales 2010
+// Distributed under the Boost Software Licence, Version 1.0.
+// (See accompanying file LICENCE_1_0.txt or copy at
+// http://www.boost.org/LICENCE_1_0.txt)
+
+#endif 
