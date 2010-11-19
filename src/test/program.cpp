3bcea11f7ef6133fb821e566110eaec1a2ed228f
Scott Wales <wales@physics.usyd.edu.au>
2010-11-19 20:37:35 +1100

    Program test works

--- src/test/program.cpp
+++ src/test/program.cpp
@@ -5,21 +5,32 @@
 
 #include <opencl-cxx/program.hpp>
 #include <opencl-cxx/context.hpp>
+#include <opencl-cxx/device.hpp>
 #include <sstream>
 #include <iostream>
+#include <boost/exception.hpp>
 
 using namespace OpenCL;
 
 int main(){
-    Context context;
+    try {
+        Context context;
+        std::vector<Device> devices = context.getAllDevices();
+        for (size_t i=0;i<devices.size();++i){
+            std::cout<<devices[i].Name()<<"\n";
+        }
 
-    std::string source = "__kernel void test(int a){a*=2;}";
-    std::stringstream stream(source);
+        std::string source = "__kernel void test(int a){a*=2;}";
+        std::stringstream stream(source);
 
-    Program program(context,stream);
-    std::cout<<program.Source()<<"\n";
-    program.Build();
+        Program program(context,stream);
+        std::cout<<program.Source()<<"\n";
+       program.Build();
 
+    } catch (boost::exception& e) {
+        std::cerr<<diagnostic_information(e);
+        throw;
+    }
     return 0;
 }
 
