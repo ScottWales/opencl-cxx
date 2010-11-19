4da6f514dd8dafd341b264a5e314fdfe00419e15
Scott Wales <wales@physics.usyd.edu.au>
2010-11-19 19:32:56 +1100

    Error code implementation

--- include/opencl-cxx/error.hpp
+++ include/opencl-cxx/error.hpp
@@ -1,9 +1,38 @@
-#ifndef OPENCL_CXX_HPP
-#define OPENCL_CXX_HPP 
+#ifndef OPENCL_CXX_ERROR_HPP
+#define OPENCL_CXX_ERROR_HPP 
 
 // Copyright Scott Wales 2010
 // Distributed under the Boost Software Licence, Version 1.0.
 // (See accompanying file LICENCE_1_0.txt or copy at
 // http://www.boost.org/LICENCE_1_0.txt)
+
+#include <exception>
+#include <boost/exception.hpp>
+
+namespace OpenCL {
+
+#define CLCheck(x) CLCheckImpl(x,__FILE__,__LINE__,__func__)
+    inline void CLCheckImpl(int errcode, 
+            const char * file,
+            long long line,
+            const char * function){
+        if (errcode != 0){
+            ::boost::throw_exception( 
+                    ::boost::enable_error_info(RuntimeError(errcode)) <<
+                    ::boost::throw_function(function) <<
+                    ::boost::throw_file(file) <<
+                    ::boost::throw_line(line) );
+        }
+    }
+
+    class RuntimeError : 
+        public virtual boost::exception,
+        public virtual std::exception 
+    {
+        public:
+            RuntimeError(int errcode):errcode(errcode){}
+            int errcode;
+    };
+}
 
 #endif 
