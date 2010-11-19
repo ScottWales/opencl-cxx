c233f30e85f56026a842f84f4fbb8e440d8b5b4e
Scott Wales <wales@physics.usyd.edu.au>
2010-11-19 18:58:55 +1100

    Program header filled out

--- include/opencl-cxx/program.hpp
+++ include/opencl-cxx/program.hpp
@@ -1,9 +1,45 @@
-#ifndef OPENCL_CXX_HPP
-#define OPENCL_CXX_HPP 
+#ifndef OPENCL_CXX_PROGRAM_HPP
+#define OPENCL_CXX_PROGRAM_HPP 
 
 // Copyright Scott Wales 2010
 // Distributed under the Boost Software Licence, Version 1.0.
 // (See accompanying file LICENCE_1_0.txt or copy at
 // http://www.boost.org/LICENCE_1_0.txt)
+
+#include <opencl-cxx/opencl.hpp>
+#include <istream>
+
+namespace OpenCL {
+    class Context;
+    class Device;
+
+    class Program {
+        public:
+            Program(Context context, std::istream& stream);
+
+            Program(const Program& other);
+            Program& operator=(const Program& other);
+            ~Program();
+
+            Build();
+            Build(std::vector<Device> devices);
+
+            Context getContext();
+            std::vector<Device> getDevices();
+
+            std::vector<std::string> Source();
+            std::vector<std::vector<unsigned char> > Binaries();
+
+            std::string BuildLog();
+            std::string BuildOptions();
+            cl_build_status BuildStatus();
+
+        private:
+            Program(); // Not implemented
+            Program(cl_program impl):cl_impl(impl){}
+
+            cl_program cl_impl;
+    };
+}
 
 #endif 
