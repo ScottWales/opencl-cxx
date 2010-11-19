17f55e78af2fc358f0f54695fcd431873312f7f0
Scott Wales <wales@physics.usyd.edu.au>
2010-11-19 18:46:26 +1100

    Test device properties

--- src/test/device.cpp
+++ src/test/device.cpp
@@ -4,6 +4,7 @@
 // http://www.boost.org/LICENCE_1_0.txt)
 
 #include <opencl-cxx/device.hpp>
+#include <iostream>
 
 int main(){
     // Construction
@@ -11,6 +12,20 @@ int main(){
     OpenCL::Device device2(device1);
     OpenCL::Device device3 = OpenCL::Device::Default(OpenCL::DeviceType::GPU);
     device2 = device3;
+
+    std::vector<OpenCL::Device> devices = 
+        OpenCL::Device::All(OpenCL::DeviceType::All);
+    for (size_t i=0;i<devices.size();++i){
+        std::cout<<devices[i].Name()<<"  |  "
+           <<devices[i].Vendor()<<"  |  "
+           <<devices[i].Version()<<"  |  "
+           <<devices[i].DriverVersion()<<"  |  "
+           <<devices[i].Profile()<<"\n";
+        std::vector<std::string> extensions = devices[i].Extensions();
+        for (size_t j=0;j<extensions.size();++j){
+            std::cout<<"\t"<<extensions[j]<<"\n";
+        }
+    }
 
     return 0;
 }
