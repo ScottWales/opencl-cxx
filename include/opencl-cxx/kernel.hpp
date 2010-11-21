1d2a6293778fece425ae1c661d043b3f34c0ea59
Scott Wales <wales@physics.usyd.edu.au>
2010-11-21 12:02:11 +1100

    Put variadic templates into kernel.hpp

--- include/opencl-cxx/kernel.hpp
+++ include/opencl-cxx/kernel.hpp
@@ -6,6 +6,12 @@
 // (See accompanying file LICENCE_1_0.txt or copy at
 // http://www.boost.org/LICENCE_1_0.txt)
 
+#include <opencl-cxx/opencl.hpp>
+#include <opencl-cxx/error.hpp>
+#if ( __cplusplus <= 199711L )
+#warning <openncl-cxx/kernel.hpp> uses C++0x variadic templates for full functionality
+#endif
+
 namespace OpenCL {
     class Kernel {
         public:
@@ -15,23 +21,44 @@ namespace OpenCL {
             ~Kernel();
 
             template <typename T>
-            SetArg(T arg, unsigned int index);
+                Kernel& SetArg(unsigned int index, T arg);
 
+#if ( __cplusplus > 199711L )
             // This is using C++0x variadic templates
+            template <typename T, typename... Args>
+                Kernel& SetArg(unsigned int index,
+                        T arg1, Args... rest);
+
             template <typename... Args>
                 Kernel& operator()(Args&... args){
-                    return RecursiveSetArg(0,args...);
+                    return SetArg(0,args...);
                 }
+#endif
+
         private:
             Kernel(); // Not implemented
             Kernel(cl_kernel impl):cl_impl(impl){}
 
-            template <typename T, typename... Args>
-                Kernel& RecursiveSetArg(unsigned int index,
-                        T arg1, Arg... rest);
-
             cl_kernel cl_impl;
     }
+
+#if ( __cplusplus > 199711L )
+    template <typename T, typename... Args>
+        inline Kernel& Kernel::SetArg(unsigned int index,
+                T arg1, Args... rest){
+            SetArg(index,arg1);
+            return SetArg(index+1,rest...);
+        }
+#endif
+    template <typename T>
+        inline Kernel& Kernel::SetArg(unsigned int index,
+                T arg){
+            CLCheck(clSetKernelArg(cl_impl,
+                        index,
+                        sizeof(arg),
+                        &arg));
+            return *this;
+        }
 }
 
 #endif 
