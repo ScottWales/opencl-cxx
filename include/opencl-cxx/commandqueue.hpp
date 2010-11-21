c5793d6c419314c6ef1c9deb02d59f573c05ba49
Scott Wales <wales@physics.usyd.edu.au>
2010-11-21 12:42:52 +1100

    Command queue header

--- include/opencl-cxx/commandqueue.hpp
+++ include/opencl-cxx/commandqueue.hpp
@@ -1,9 +1,46 @@
-#ifndef OPENCL_CXX_HPP
-#define OPENCL_CXX_HPP 
+#ifndef OPENCL_CXX_COMMANDQUEUE_HPP
+#define OPENCL_CXX_COMMANDQUEUE_HPP 
 
 // Copyright Scott Wales 2010
 // Distributed under the Boost Software Licence, Version 1.0.
 // (See accompanying file LICENCE_1_0.txt or copy at
 // http://www.boost.org/LICENCE_1_0.txt)
+
+#include <opencl-cxx/opencl.hpp>
+#include <vector>
+
+namespace OpenCL {
+    class Event;
+    class Context;
+    class Device;
+
+    class CommandQueue {
+        public:
+            CommandQueue(Context context);
+            CommandQueue(Context context, Device device);
+
+            CommandQueue(const CommandQueue& other);
+            CommandQueue& operator=(const CommandQueue& other);
+            ~CommandQueue();
+
+            /// Add a single action
+            template <typename T>
+                std::vector<Event> Add(T action);
+
+            /// Add a list of actions to be evaluated in sequence
+            template <typename T, typename Args...>
+                std::vector<Event> Add(T action1, Args... rest);
+
+            template <typename Args...>
+                std::vector<Event> operator()(Args... args){
+                    return Add(args...);
+                }
+        private:
+            CommandQueue(); // Not implemented
+            CommandQueue(cl_commandqueue impl):cl_impl(impl){};
+
+            cl_commandqueue impl;
+    };
+}
 
 #endif 
